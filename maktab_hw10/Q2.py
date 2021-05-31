0cc740188a8800371f71b5a9529c0f1fd21410b2
AmirHossein <a.h.ansari.flv@gmail.com>
2021-05-31 13:01:46 +0430

    answer Q2

---
+++ maktab_hw10/Q2.py
@@ -0,0 +1,35 @@
+import json
+
+
+class User(json.JSONEncoder):
+    def __init__(self, name, age, pas, phone, email=None):
+        self.name = name
+        if isinstance(age, int):
+            self.age = age
+        else:
+            raise Exception
+        self.pas = pas
+        if phone.startswith('09') and len(phone) == 11:
+            self.phone = phone
+        else:
+            raise Exception
+        self.email = email
+
+    def default(self, obj):
+        if isinstance(obj, User):
+            return {
+                'name': self.name, 'age': self.age, 'password': self.pas, 'phone': self.phone, 'email': self.email}
+
+        else:
+            return super().default(obj)
+
+    def register(self, ):
+        with open('Q2.json', 'a')as f:
+            json.dump(self.default(self), f, indent=4, separators=(",", ":"))
+            f.write(',')
+
+
+us = User('amir', 12, 'amir67', '09011111111')
+us1 = User('ali', 19, 'ali98', '09011111122')
+us.register()
+us1.register()
