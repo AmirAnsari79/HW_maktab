2aa13764db9bf3f7391e970cacd56701e3ca42f1
AmirHossein <a.h.ansari.flv@gmail.com>
2021-06-06 18:05:55 +0430

    answer Q4 seri 9 maktab52

--- maktab_hw9/Q4.py
+++ maktab_hw9/Q4.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://virgool.io/@asamasach/%DA%86%D8%B1%D8%A7-%D8%A7%D8%B2-%D8%AF%D8%A7%D9%86%D8%B4%DA%AF%D8%A7%D9%87-%D9%85%D8%AA%D9%86%D9%81%D8%B1%D9%85-yj3zebvieybc'
-
+# comment
 res = requests.get(url)
 bs = BeautifulSoup(res.text, 'html.parser')
 # x = bs.find('p', {'class': 'md-block-unstyled md-block-rtl'})
